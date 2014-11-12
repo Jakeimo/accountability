304ca0a80414f32f04c50bc3f089324c9ce5bbf2
Jakeimo <Jakeimo@gmail.com>
2014-11-12 15:39:53 +1100

    Initial commit

---
+++ action.php
@@ -0,0 +1,33 @@
+<?php 
+$mysqli = new mysqli("localhost", "root", "password", "personal_data_testing");
+
+//Check Connection
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+//Create local variables for insertion
+$date = $_POST["date"];
+$studyLength = $_POST["study"];
+$moneySpent = $_POST["spend"];
+$moneyEarnt = $_POST["earnt"];
+$happy = $_POST["happiness"];
+
+//Prepare sql statements, and bind parameters
+$stmtStudy = $mysqli->prepare(
+	"INSERT INTO study (date, length) VALUES (?, ?)");
+$stmtStudy->bind_param("sd", $date, $studyLength);
+
+$stmtMoney = $mysqli->prepare(
+	"INSERT INTO money (date, spent, earnt) VALUES (?, ?, ?)");
+$stmtMoney->bind_param('sdd', $date, $moneySpent, $moneyEarnt);
+
+$stmtHappy = $mysqli->prepare(
+	"INSERT INTO happiness (date, rating) VALUES (?, ?)");
+$stmtHappy->bind_param('sd', $date, $happy);
+
+$stmtHappy->execute();
+$stmtMoney->execute();
+$stmtStudy->execute();
+
+?>
+
