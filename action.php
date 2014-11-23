daa05bf05c5bbbf21ad1d4b19aee3b7c1e775d9d
Jakeimo <Jakeimo@gmail.com>
2014-11-23 12:07:25 +1100

    Update check for entered day, to reflect updated database structure

--- action.php
+++ action.php
@@ -1,16 +1,18 @@
 <?php  
 //$database = "personal_data";
 $database = "personal_data_testing";
+/*$database = "jspikxqd_personal_data";
+$database = "jspikxqd_personal_data_testing";*/
 
-/*$server = "localhost";
+$server = "localhost";
 $user = "root";
-$password = "password";*/
-$server = "jspi.me";
+$password = "password";
+/*$server = "jspi.me";
 $user = "jspikxqd";
-$password = "C8Ri_bCbOYFs2";
+$password = "C8Ri_bCbOYFs2"; */
 $mysqli = new mysqli($server, $user, $password, $database);
 
-Check Connection
+//Check Connection
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
@@ -21,6 +23,18 @@ $moneySpent = $_POST["spend"];
 $moneyEarnt = $_POST["earnt"];
 $happy = $_POST["happiness"];
 
+$dateCheckStmt = $mysqli->prepare(
+	"SELECT id FROM dayData WHERE date=?");
+$dateCheckStmt->bind_param("s", $date);
+$dateCheckStmt->execute();
+$dateCheckStmt->bind_result($id);
+$dateCheckStmt->fetch();
+
+if($id !== 0){ //An entry for this date has already been found
+	setcookie('Insertion', 'fail', time() + 2);
+	header('Location: index.html'); 
+	die();
+} else { 
 //Prepare sql statements, and bind parameters
 $stmt = $mysqli->prepare(
 	"INSERT INTO dayData (date, studyLength, moneySpent, moneyEarnt, happyRating) VALUES (?, ?, ?, ?, ?)");
@@ -28,13 +42,8 @@ $stmt->bind_param("sdddi", $date, $studyLength, $moneySpent, $moneyEarnt, $happy
 
 $stmt->execute();
 
-if($mysqli->affected_rows != 1){
-	//printf("Insertion failed. Rows Affected: %d\n", $mysqli->affected_rows);
-	setcookie('Insertion', 'fail', time() + 2);
-	header('Location: index.html'); 
-} else {
-	setcookie('Insertion', 'success', time() + 2);
-	header('Location: index.html');
+setcookie('Insertion', 'success', time() + 2);	
+header('Location: index.html');
 }
 ?>
 
